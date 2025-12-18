12466800972c966a352b4181c83d4eaa3a33cc3b
mjmicro <manoj kumar>
2025-12-18 23:09:29 +0530

    fix(monodog): updated installation script

---
+++ packages/monoapp/dist/cli.js
@@ -0,0 +1,19 @@
+"use strict";
+// #!/usr/bin/env node
+// import { init } from './commands/init';
+Object.defineProperty(exports, "__esModule", { value: true });
+// const [, , command, ...args] = process.argv;
+// switch (command) {
+//   case 'init':
+//     init(args);
+//     break;
+//   default:
+//     console.log(`
+// Monodog CLI
+// Usage:
+//   monodog init <app-name>
+// Example:
+//   pnpm dlx @mindfiredigital/monodog init my-app
+// `);
+//     process.exit(0);
+// }

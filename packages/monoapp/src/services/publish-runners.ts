46e68b68fff2c6433806717cfeb190da5e9a5701
mjmicro <manoj kumar>
2026-03-04 23:45:54 +0530

    chore: release @monodog/ci-status@1.3.3

--- packages/monoapp/src/services/publish-runners.ts
+++ packages/monoapp/src/services/publish-runners.ts
@@ -211,7 +211,7 @@ export class NodePublishRunner extends PublishRunner {
 
       const tagName = `${packageName}-v${version}`;
       await execSync(
-        `git add . && git commit -m "chore: release ${packageName}@${version}" --no-verify`
+        `git add :/ && git commit -m "chore: release ${packageName}@${version}" --no-verify`
       );
       await execSync(`git tag ${tagName}`);
     } catch (error) {
